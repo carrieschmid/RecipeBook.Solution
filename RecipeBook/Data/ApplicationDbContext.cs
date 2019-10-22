e77d927c2f7ea92ccf1edefdf944c371b7e11a36
Jennifer Batara and carrie schmid <jennifer+carrie@>
2019-10-22 16:22:49 -0700

    Add db application context to template file

--- RecipeBook/Data/ApplicationDbContext.cs
+++ RecipeBook/Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RecipeBook.Models;
 
-namespace RecipeBook.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+namespace RecipeBook.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public virtual DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+        public DbSet<RecipeCategory> RecipeCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
     }
 }
